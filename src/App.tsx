3b7de85ac3dfaeae3489254c5059757e2e138265
Genaro Garcia <genaro.dev@outlook.com>
2025-03-18 09:54:07 -0500

    wip: add semantic HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  return (
+    <main>
+      <section>
+        <h1>Desserts</h1>
+        {/* Products List */}
+      </section>
+      {/* Cart */}
+      <aside>
+        <h2>Your Cart ()</h2>
+      </aside>
+    </main>
+  );
 }
 
 export default App;
